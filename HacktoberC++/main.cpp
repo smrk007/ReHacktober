c47d3af025dfdbd9f6f70ededd0a44a749db3bfe
smrk007 <skenne5@tigers.lsu.edu>
2017-10-21 14:52:06 -0500

    Initial Commit

---
+++ HacktoberC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HacktoberC++
+//
+//  Created by Sean Michael Ritschard Kennedy on 10/21/17.
+//  Copyright © 2017 skware. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
