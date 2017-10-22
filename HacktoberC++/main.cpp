d0849f82105b8069fe5ffbde35144f6e0f5ba22b
Sean Kennedy <smrk007@users.noreply.github.com>
2017-10-21 23:25:30 -0500

    Update main.cpp

--- HacktoberC++/main.cpp
+++ HacktoberC++/main.cpp
@@ -1,15 +1,45 @@
-//
-//  main.cpp
-//  HacktoberC++
-//
-//  Created by Sean Michael Ritschard Kennedy on 10/21/17.
-//  Copyright © 2017 skware. All rights reserved.
-//
-
+// Frameworks
 #include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <time.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// Header Classes
+#include "util.hpp"
+#include "Object.hpp"
+#include "player.hpp"
+#include "game.hpp"
+
+using namespace std;
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800,600), "Test Window");
+    
+    Game game = Game();
+    
+    while (window.isOpen())
+    {
+        // Updates
+        
+        game.update(0.1, window);
+        
+        // Rendering
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+        
+        window.clear(sf::Color(0,0,0));
+        
+        game.draw(window);
+        
+        window.display();
+    }
+    
     return 0;
 }
